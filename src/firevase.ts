10c25e93f473b0a7d4e157751775cc6dfcd19dd5
WhackoCoding <whackocoding@gmail.com>
2024-07-25 01:52:32 +0900

    SETUP

---
+++ src/firevase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC39ztU0xDCnS6HoMvk6m6e7xhnAQqruVw",
+  authDomain: "twcwitter.firebaseapp.com",
+  projectId: "twcwitter",
+  storageBucket: "twcwitter.appspot.com",
+  messagingSenderId: "882848254096",
+  appId: "1:882848254096:web:436fc4038a9df2e359f015"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
